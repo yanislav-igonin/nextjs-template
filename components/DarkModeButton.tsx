29cc9808e30f3700d76fc8bfa110095fc42a2d55
Yanislav Igonin <yanislavdabest@gmail.com>
2022-06-10 00:37:37 +0300

    refac

--- components/DarkModeButton.tsx
+++ components/DarkModeButton.tsx
@@ -13,7 +13,7 @@ const darkModePath = <path
   strokeWidth={2}
   d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
 />;
-const whiteColor = 'text-white';
+const lightColor = 'text-white';
 const darkColor = 'text-slate-600';
 
 export const DarkModeButton = () => {
@@ -27,7 +27,7 @@ export const DarkModeButton = () => {
   useEffect(() => {
     const nextPath = isDarkNow ? darkModePath : lightModePath;
     setPath(nextPath);
-    const nextColor = isDarkNow ? whiteColor : darkColor;
+    const nextColor = isDarkNow ? lightColor : darkColor;
     setColor(nextColor);
   }, [isDarkNow]);
 
