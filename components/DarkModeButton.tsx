55f48290e10c7ba6a5298399da339be79edf07dc
Yanislav Igonin <yanislavdabest@gmail.com>
2022-06-10 00:34:35 +0300

    refac

--- components/DarkModeButton.tsx
+++ components/DarkModeButton.tsx
@@ -7,17 +7,18 @@ const lightModePath = <path
   strokeWidth={2}
   d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
 />;
-
 const darkModePath = <path
   strokeLinecap="round"
   strokeLinejoin="round"
   strokeWidth={2}
   d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
 />;
+const whiteColor = 'text-white';
+const darkColor = 'text-slate-600';
 
 export const DarkModeButton = () => {
   const [theme, setTheme] = useDarkMode();
-  const [color, setColor] = useState('text-slate-600');
+  const [color, setColor] = useState(darkColor);
   const [path, setPath] = useState(lightModePath);
 
   const isDarkNow = theme === ThemeMode.Dark;
@@ -26,7 +27,7 @@ export const DarkModeButton = () => {
   useEffect(() => {
     const nextPath = isDarkNow ? darkModePath : lightModePath;
     setPath(nextPath);
-    const nextColor = isDarkNow ? 'text-white' : 'text-slate-600';
+    const nextColor = isDarkNow ? whiteColor : darkColor;
     setColor(nextColor);
   }, [isDarkNow]);
 
